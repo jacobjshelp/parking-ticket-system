ec5571596a9d0cdf9777f2ac0b95d3c09f5dec92
jacobjshelp <jacobjshelp>
2024-08-28 16:02:55 +0400

    Step 6

--- src/TicketMachine.ts
+++ src/TicketMachine.ts
@@ -1,33 +1,10 @@
-import fs from "node:fs";
-
 export default class TicketMachine {
   private currency: string;
   private pricePerMinute: number;
 
-  private timeout = 7;
-
   constructor(_currency: string, _pricePerMinute: number) {
     this.currency = _currency;
     this.pricePerMinute = _pricePerMinute;
-  }
-
-  async startInteractionReadFile(): Promise<void> {
-    console.log("Enter your wanted parking duration in minutes in the file called duration.txt");
-    await this.sleep();
-
-    let duration: number | undefined = undefined;
-    while (duration === undefined) {
-      duration = await this.attemptToReadFile();
-    }
-    const price = this.calculatePrice(duration);
-    this.displayPrice(price);
-  }
-
-  endInteractionReadFile(): void {
-    fs.writeFile("./data/duration.txt", "", function () {
-      console.log("Duration file has been reset");
-      console.log("Thank you for parking with us!");
-    });
   }
 
   private displayPrice(price: number) {
@@ -36,27 +13,5 @@ export default class TicketMachine {
 
   private calculatePrice(duration: number) {
     return duration * this.pricePerMinute;
-  }
-
-  private sleep() {
-    return new Promise((resolve) => {
-      console.log(`Waiting ${this.timeout} seconds`);
-      setTimeout(resolve, this.timeout * 1000);
-    });
-  }
-
-  private async attemptToReadFile(): Promise<undefined | number> {
-    try {
-      const data = fs.readFileSync("./data/duration.txt", "utf8");
-      if (data === "") {
-        console.log("The file is still empty, please input a duration");
-        await this.sleep();
-      } else {
-        return Number(data);
-      }
-    } catch (err) {
-      console.error(err);
-      return undefined;
-    }
   }
 }
